1ef6b5039979ec53fb5f82599c7cb55288dfac6e
Midnightlight <vegiangle2602@gmail.com>
2021-10-07 16:54:17 +0300

    porting the project from git.vamk.fi to github.com

---
+++ src/main/resources/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package fi.vamk.e1800927.northwind;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+        protected void configure(HttpSecurity http) throws Exception {
+                http.csrf().disable().authorizeRequests().antMatchers("/", "/index").permitAll().anyRequest().authenticated().and().httpBasic();                                .and().httpBasic();
+        }
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.ldapAuthentication().userSearchFilter("uid={0}").contextSource()
+				.url("ldap://ldap.puv.fi:389/dc=puv,dc=fi");
+	}
+
+	/*
+	 * old version using application.properties and
+	 * spring.ldap.embedded.ldif=classpath:test-server.ldif
+	 * 
+	 * @Override public void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception { auth .ldapAuthentication() .userDnPatterns("uid={0},ou=people")
+	 * .groupSearchBase("ou=groups") .contextSource(contextSource())
+	 * .passwordCompare() .passwordEncoder(new LdapShaPasswordEncoder())
+	 * .passwordAttribute("userPassword"); }
+	 * 
+	 * @Bean public DefaultSpringSecurityContextSource contextSource() { return new
+	 * DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"),
+	 * "dc=springframework,dc=org"); }
+	 */
+}
